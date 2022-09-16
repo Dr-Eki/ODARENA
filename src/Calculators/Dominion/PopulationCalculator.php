1c1941f1c7fdb94192a9c940d5b373f7028b693d
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2022-09-16 18:05:07 +0300

    Nomad

--- src/Calculators/Dominion/PopulationCalculator.php
+++ src/Calculators/Dominion/PopulationCalculator.php
@@ -159,6 +159,7 @@ class PopulationCalculator
         $housingPerBarrenAcre = 5;
         $housingPerBarrenAcre += $dominion->race->getPerkValue('extra_barren_max_population');
         $housingPerBarrenAcre += $dominion->race->getPerkValue('extra_barren_housing_per_victory') * $this->statsService->getStat($dominion, 'invasion_victories');
+        $housingPerBarrenAcre += $dominion->race->getPerkValue('extra_barren_housing_per_net_victory') * max(($this->statsService->getStat($dominion, 'invasion_victories') - $this->statsService->getStat($dominion, 'defense_failures')), 0);
 
         foreach ($this->landHelper->getLandTypes($dominion) as $landType)
         {
