04d9a67396fa74a81bb0dabaeed9d830105f7efd
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2023-01-14 06:28:59 +0200

    Update WorldNewsService.php

--- src/Services/Dominion/WorldNewsService.php
+++ src/Services/Dominion/WorldNewsService.php
@@ -100,7 +100,7 @@ class WorldNewsService
             $event->source_type == Dominion::class and $event->source_id == $dominion->id or
             $event->target_type == Dominion::class and $event->target_id == $dominion->id or
             $event->source_type == Dominion::class and $event->source->realm->id == $dominion->realm->id or
-            $event->target_type == Dominion::class and $event->target->realm->id == $dominion->realm->id or
+            #$event->target_type == Dominion::class and $event->target->realm->id == $dominion->realm->id or
 
             $event->target_type == Realm::class and $event->target_id == $dominion->realm->id or
             $event->target_type == Realm::class and $event->target_id == $dominion->realm->id
