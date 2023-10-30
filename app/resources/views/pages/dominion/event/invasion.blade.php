87d429372493d6931c3edff6f345e535f645525c
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2023-10-30 10:57:14 +0200

    Update invasion.blade.php

--- app/resources/views/pages/dominion/event/invasion.blade.php
+++ app/resources/views/pages/dominion/event/invasion.blade.php
@@ -492,9 +492,9 @@
                                         <th colspan="2">Ore Exhaustion</th>
                                     </tr>
                                     <tr>
-                                        @if($even->source->race->name == 'Yeti')
+                                        @if($even->source->race->key == 'yeti')
                                             <td colspan="2"><small class="text-muted">Stonethrowers darken the sky with boulders.</small></td>
-                                        @elseif($even->source->race->name == 'Gnome')
+                                        @elseif($even->source->race->key == 'gnome')
                                             <td colspan="2"><small class="text-muted">As blasts of gunpowder are heard across the battlefield, projectiles of ore rain down on the enemy.</small></td>
                                         @endif
                                     </tr>
