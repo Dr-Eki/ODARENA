832a89dc4430c9a95b7cbb312f8d6234a880b6cf
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2024-02-20 19:43:39 +0200

    Create 2024_02_20_174215_add_type_to_round_winners_table.php

---
+++ app/database/migrations/2024_02_20_174215_add_type_to_round_winners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTypeToRoundWinnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('round_winners', function (Blueprint $table) {
+            $table->text('type')->before('data');           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('round_winners', function (Blueprint $table) {
+            $table->dropColumn([
+                'type',
+            ]);
+        });
+    }
+}
