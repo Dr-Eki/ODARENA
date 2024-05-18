01b4429faf466009571e35c6738d27bda2527dcc
Dr. Eki <37151775+Dr-Eki@users.noreply.github.com>
2024-05-18 14:16:56 +0300

    Update ProcessDominionJob.php

--- src/Jobs/ProcessDominionJob.php
+++ src/Jobs/ProcessDominionJob.php
@@ -875,21 +875,6 @@ class ProcessDominionJob implements ShouldQueue
 
             xtLog("[{$dominion->id}] *** {$row->amount} {$row->resource} completed and set for tick.");
         }
-
-        $rowsToDelete = DB::table('dominion_queue')
-            ->where('dominion_id', $dominion->id)
-            ->where('hours', '<=', 0)
-            ->get();
-
-        $rowToDeleteCount = count($rowsToDelete);
-
-        DB::table('dominion_queue')
-            ->whereIn('id', $rowsToDelete)
-            ->delete();
-
-        xtLog("[{$dominion->id}] *** Deleted {$rowToDeleteCount} completed queue items.");
-
-
     }
-    
+
 }
